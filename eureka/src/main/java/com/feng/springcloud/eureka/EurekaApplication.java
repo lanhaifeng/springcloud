a58338ac8468557cc0de5d99b29791b2b2976134
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-12-23 15:14:47 +0800

    eureka服务端docker镜像制作

---
+++ eureka/src/main/java/com/feng/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.feng.springcloud.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * eureka
+ * 2019/12/23 14:08
+ * eureka服务端启动入口
+ *
+ * @author lanhaifeng
+ * @since 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
