962efbdb656ee638cf23a07a397073a82007bc23
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-12-23 20:53:08 +0800

    eureka服务端docker镜像制作

--- eureka/src/main/java/com/feng/springcloud/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/feng/springcloud/eureka/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.feng.springcloud.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -16,6 +17,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
